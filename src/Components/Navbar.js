7820ac2a4ebff9a6ab4657cdfc4481e3bfb31c24
butlerpaul1 <butlerpaul1@hotmail.com>
2021-02-20 18:31:49 +0000

    moving components within their own dir

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,10 +2,10 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Home Page</h1>
+      <h1>Home</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">Create a new blog</a>
+        <a href="/create">Create New Blog</a>
       </div>
     </nav>
   );
