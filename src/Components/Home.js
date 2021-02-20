6a58161d9ad5f3874b40651523ca315c1c7ae728
butlerpaul1 <butlerpaul1@hotmail.com>
2021-02-20 18:55:14 +0000

    remove delete functionality

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,26 +1,28 @@
 //home page template
 import { useState, useEffect} from 'react'
 import BlogList from './BlogList';
+import config from '../config/config.json'
 
 const Home = () => {
   //functions 
-
-  const [blogs, setBlogs] = useState([
-  ]);
-
-  const handleDelete = (id) => {
-    const filteredBlogs = blogs.filter( blog => blog.id !== id);
-    setBlogs(filteredBlogs);
-  }
+  const [blogs, setBlogs] = useState([]);
 
   //run when on re-render
   useEffect(() => {
-
-  });
+    fetch(config.apiUrl + "blogs/")
+    .then(res => { //get the result from the api
+      return res.json()
+    }) // get the data from the result
+    .then((data) => {
+      setBlogs(data);
+    })
+    //only fire once on first render
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={ handleDelete } />
+      {/* only render blog list if it isn't null */}
+      {blogs && <BlogList blogs={blogs}/>}
     </div>
   );
 }
