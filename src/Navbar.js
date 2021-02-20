7fa02a2f10944a4fdde5fd9541a98112b57d9e95
butlerpaul1 <butlerpaul1@hotmail.com>
2021-02-20 14:46:02 +0000

    add navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Home Page</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create" style={{}}>Create a new blog</a>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar
