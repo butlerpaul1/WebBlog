994511e6360b1317c8caf31ba073342c40ae0f84
butlerpaul1 <butlerpaul1@hotmail.com>
2021-02-28 18:45:21 +0000

    add form to create blog

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [body, setBody] = useState('');
+
   return (
     <div className="create">
       <h2>Create a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <label>Blog author:</label>
+        <select required value={author} onChange={(e) => setAuthor(e.target.author)}>
+          <option value="Elon Musk">Elon Musk</option>
+          <option value="Bill Gates">Bill Gates</option>
+          <option value="Steve Jobs">Steve Jobs</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
