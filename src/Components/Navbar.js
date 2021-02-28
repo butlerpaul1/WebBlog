1219388f335486c602fea836ba319bdeec82bc4a
butlerpaul1 <butlerpaul1@hotmail.com>
2021-02-28 16:50:52 +0000

    add react linking to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,13 @@
 ///Nav Bar Menu
+import {Link} from 'react-router-dom'
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Home</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">Create New Blog</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create New Blog</Link>
       </div>
     </nav>
   );
