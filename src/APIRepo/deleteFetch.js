ac4aaafa5e93e3b685838312eb492dfac26cba72
butlerpaul1 <butlerpaul1@hotmail.com>
2021-03-21 16:06:36 +0000

    add delete endpoint

---
+++ src/APIRepo/deleteFetch.js
@@ -0,0 +1,21 @@
+//fetch the data from the server
+const useDeleteFetch = (url, blog) => {
+  const abortCont = new AbortController();
+
+  setTimeout(() => {
+    fetch(url, {
+      method: 'DELETE',
+      signal: abortCont.signal
+    })
+      .then(res => { //get the result from the api
+        if (!res.ok) { // error coming back from server
+          throw Error('could not create the blogs for that resource');
+        }
+      }, 1000);
+
+    // abort the fetch
+    return () => abortCont.abort();
+
+  })
+}
+export default useDeleteFetch;
