cd4a5a281126ea38d415f50242f2086f75657ca1
butlerpaul1 <butlerpaul1@hotmail.com>
2021-03-21 16:06:47 +0000

    add delete button

--- src/Components/BlogDetails.js
+++ src/Components/BlogDetails.js
@@ -1,11 +1,19 @@
 import { useParams } from "react-router";
 import useFetch from "../APIRepo/useFetch";
+import deleteFetch from "../APIRepo/deleteFetch";
 import config from '../config/config.json';
+import { useHistory } from 'react-router-dom';
 
 const BlogDetails = () => {
   //get the id from the route
   const { id } = useParams();
+  const history = useHistory();
   const { isLoading, blogs: blog, error } = useFetch(config.apiUrl + "blogs/" + id);
+
+  const handleDelete = () => {
+    deleteFetch(config.apiUrl + "blogs/" + blog.id);
+    history.push('/');
+  }
 
   return (
     <div className="blog-details">
@@ -17,6 +25,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written By: {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete Blog</button>
         </article>
       }
 
